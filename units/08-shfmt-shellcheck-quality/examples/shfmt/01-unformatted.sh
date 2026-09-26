bc9ee73d2ad4d6ccb4d968fa5a38f2094dfd7391
seiya matsuoka <seiya.matsuoka0413@gmail.com>
2026-09-27 00:24:30 +0900

    feat: Unit 08 で使用する Shell Script のサンプルファイルを編集

--- units/08-shfmt-shellcheck-quality/examples/shfmt/01-unformatted.sh
+++ units/08-shfmt-shellcheck-quality/examples/shfmt/01-unformatted.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-greet() {
-    local name=$1
-    if [[ -n $name ]]; then
-        printf 'hello, %s\n' "$name"
-    else
-        printf '%s\n' 'hello'
-    fi
+greet(){
+local name=$1
+if [[ -n $name ]];then
+printf 'hello, %s\n' "$name"
+else
+printf '%s\n' 'hello'
+fi
 }
 
 greet "${1:-}"
